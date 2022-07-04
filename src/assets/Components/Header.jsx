ae67aa091abb4f02d05af59f8989be75f5f88211
Axel <axel.siriez@gmail.com>
2022-07-04 10:07:06 +0200

    set up dark mode

--- src/assets/Components/Header.jsx
+++ src/assets/Components/Header.jsx
@@ -1,7 +1,10 @@
+import Nav from "./Nav"
+
 export default function Header() {
     return (
         <header>
             <nav className="absolute w-full">
+            <Nav />
                 <ul className="flex justify-evenly p-8 bg-gradient-to-r from-cyan-500 to-blue-500">
                     <li className="text-white text-3xl cursor-pointer hover:bg-black hover:bg-opacity-20 p-2 rounded-lg">My Projects</li>
                     <li className="text-white text-3xl cursor-pointer hover:bg-black hover:bg-opacity-20 p-2 rounded-lg">My skills</li>
