ea2abefdb7fda65bfdb97db5c0a7698abf417bfd
Axel <axel.siriez@gmail.com>
2022-07-06 15:01:24 +0200

    tr:scale

--- src/assets/Components/About.jsx
+++ src/assets/Components/About.jsx
@@ -7,12 +7,12 @@ export default function About(ref) {
     return (
         <div className="h-screen" ref={aboutSection}>
             <div className="h-screen flex">
-                <div className="w-1/2 flex justify-center items-center">
+                <div className="w-1/2 flex justify-center items-center ">
                         <span className="border-solid border-b-2 w-44 border-cyan-600 "></span>
                         <h2 className="text-6xl dark:text-white duration-500 ml-8">About me</h2>
                 </div>
                 <div className="w-1/2 flex flex-col items-center justify-center gap-12">
-                    <p className="w-2/4 text-2xl text-white duration-500 bg-cyan-500 rounded-md p-6 px-10 shadow-2xl leading-10 ">
+                    <p className="w-2/4 text-2xl text-white duration-500 bg-cyan-500 rounded-md p-6 px-10 shadow-2xl leading-10 hover:scale-110">
                         Hello everyone,
                         After 3 years dedicated to animal care, I decided to reorient myself towards my other passion: web development. I particularly appreciate this job because it allows me to learn continuously, to see the evolution of my work (web design) and to use my resourcefulness and my thirst to surpass myself every day.
                     </p>
