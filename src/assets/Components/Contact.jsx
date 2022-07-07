fac587acccfb7ae616b053dcd73c65a3bde1c133
Axel <axel.siriez@gmail.com>
2022-07-07 09:58:20 +0200

    contact form

--- src/assets/Components/Contact.jsx
+++ src/assets/Components/Contact.jsx
@@ -1,17 +1,82 @@
-export default function Contact(){
-    return(
+export default function Contact() {
+    return (
         <div className="h-screen">
-            <div className="w-11/12 flex items-center pt-36">
+            <div className="w-11/12 flex items-center pt-36 mb-16">
                 <h2 className="text-6xl dark:text-white duration-500 px-8">Contact</h2>
                 <span className="border-solid border-b-2 w-44 border-cyan-600 "></span>
             </div>
-            <div className="h-3/4 flex flex-col items-center justify-center">
-                <form action="#" method="GET" className="flex flex-col gap-8">
-                    <input type="text" name="name" placeholder="Name"/>
-                    <input type="mail" name="name" placeholder="e-mail"/>
-                    <input type="text" name="message" placeholder="Message" />
-                </form>
+            <div class="w-full md:w-96 md:max-w-full mx-auto shadow-xl">
+                <div class="p-6 bg-cyan-500 border-gray-300 sm:rounded-md">
+                    <form method="POST" action="#">
+                        <label class="block mb-6">
+                            <span class="text-gray-700 text-lg">Your name</span>
+                            <input
+                                type="text"
+                                name="name"
+                                class="block
+                                w-full
+                                mt-1
+                                border-gray-300
+                                rounded-md
+                                shadow-sm
+                                p-3
+                                outline-none"
+                                placeholder="Joe Bloggs"
+                            />
+                        </label>
+                        <label class="block mb-6">
+                            <span class="text-gray-700 text-lg">Email address</span>
+                            <input
+                                name="email"
+                                type="email"
+                                class="block
+                                        w-full
+                                        mt-1
+                                        border-gray-300
+                                        rounded-md
+                                        shadow-sm
+                                        outline-none
+                                        p-3"
+                                placeholder="joe.bloggs@example.com"
+                                required
+                            />
+                        </label>
+                        <label class="block mb-6">
+                            <span class="text-gray-700 text-lg">Message</span>
+                            <textarea
+                                name="message"
+                                class="block
+                                        w-full
+                                        mt-1
+                                        border-gray-300
+                                        rounded-md
+                                        shadow-sm
+                                        outline-none
+                                        p-3"
+                                rows="3"
+                                placeholder="Tell us what you're thinking about..."
+                            ></textarea>
+                        </label>
+                        <div class="mb-6">
+                            <button
+                                type="submit"
+                                class="h-10
+                                        px-5
+                                        text-indigo-100
+                                        bg-cyan-600
+                                        rounded-lg
+                                        transition-colors
+                                        duration-150
+                                        focus:shadow-outline
+                                        hover:bg-cyan-800"
+                            >
+                                Contact Us
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
+
         </div>
     )
 }
